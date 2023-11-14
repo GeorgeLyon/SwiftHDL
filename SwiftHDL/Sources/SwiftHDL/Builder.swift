07892e8a02d68858a6c59c4c7384603f83a0a1f6
George <GeorgeLyon@users.noreply.github.com>
2023-11-14 12:06:37 -0800

    Interim commit

--- SwiftHDL/Sources/SwiftHDL/Builder.swift
+++ SwiftHDL/Sources/SwiftHDL/Builder.swift
@@ -5,9 +5,20 @@ public class ManagedBuilder: Identifiable {
   public init(_ context: ManagedMLIRContext) async {
     self.context = context
     self.id = await context.builderNamespace.next()
+    self.cxx = context.withCxx(CxxSwiftHDL.createOpBuilder)
   }
 
   public let context: ManagedMLIRContext
   public let id: ID
   public var isValid: Bool = true
+  public var cxx: mlir.OpBuilder
+
+  public func build<Op, each Arg>(
+    _ fn: (mlir.OpBuilder, mlir.OpState) -> Op,
+    _ location: mlir.Location,
+    _ args: repeat each Arg
+  ) -> Op {
+    let state = mlir.OperationState(location)
+    fatalError()
+  }
 }
